9c49634da4879825592d018eb03f58c8cd079a86
Daisuke Okanohara <hillbig@preferred.jp>
2016-09-23 08:18:26 +0900

    008

--- 002chainer/008/008.py
+++ 002chainer/008/008.py
@@ -1,20 +1,32 @@
 from chainer import optimizers
 from chainer import links as L
+from chainer import Variable
 import numpy as np
 
 
-x = L.Linear(1, 1, nobias=True)
+class Linear(Chain):
+    def __init__(self):
+        super(MyChain, self).__init__(
+            l1=L.Linear(1, 1),
+        )
+
+    def __call__(self, x):
+        return self.l1(x)
+
+
+def f(x):
+    return 5.*x + 10
+
+x = np.linspace(-10, 10, num=1001)
+y = f(x) + 5.*np.random.randn()
+
+model = Linear()
 
 opt = optimizers.SGD()
-opt.setup(x)
-
-coef = np.zeros((1, 1), dtype=np.float32)
-print(coef.shape)
-
-
-def loss(arg):
-    return (arg(coef) - 3) ** 4
-
-for i in xrange(100):
-    opt.update(loss, x)
-    print x.W.data, loss(x).data
+opt.Setup(model)
+for epoch in xrange(100):
+    perm = np.random.perm(len(x))
+    for i in xrange(len(x)):
+        x_i = Variable(x[perm[i]])
+        y_i = Variable(y[perm[i]])
+        ## Write Here
