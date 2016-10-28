41b7845db2dea825bb5c67287c2a88df73ccc1e9
Daisuke Okanohara <daisuke.okanohara@gmail.com>
2016-10-28 14:58:51 +0900

    cat and dog

--- 003cnn/train.py
+++ 003cnn/train.py
@@ -1,15 +1,16 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+
 import chainer
-from chainer import datasets
-from chainer import links as L
-from chainer import functions as F
-from chainer import Variable, optimizers
+import chainer.functions as F
+import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 import numpy as np
-import argparse
-import math
-import random
 
+
+# Network definition
 class MLP(chainer.Chain):
 
     def __init__(self, n_units, n_out):
@@ -25,179 +26,95 @@ class MLP(chainer.Chain):
         h2 = F.relu(self.l2(h1))
         return self.l3(h2)
 
-class Block(chainer.Chain):
-    def __init__(self, in_ch, out_ch, down_sampling):
-        w = math.sqrt(2)
-        stride = 2 if down_sampling else 1
-        super(Block, self).__init__(
-            bn1=L.BatchNormalization(in_ch),
-            c1=L.Convolution2D(in_ch, out_ch, 3, pad=1, stride=stride, wscale=w, nobias=True),
-            bn2=L.BatchNormalization(out_ch),
-            c2=L.Convolution2D(out_ch, out_ch, 3, pad=1, stride=1, wscale=w, nobias=True)
-        )
-        if in_ch != out_ch:
-            self.add_link("proj", L.Convolution2D(in_ch, out_ch, 3, pad=1, stride=2, wscale=w, nobias=True))
 
-        self.in_ch = in_ch
-        self.out_ch = out_ch
-        self.use_proj = in_ch != out_ch
+def main():
+    parser = argparse.ArgumentParser(description='Chainer example: CIFAR2')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
+                        help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='',
+                        help='Resume the training from snapshot')
+    parser.add_argument('--unit', '-u', type=int, default=1000,
+                        help='Number of units')
+    args = parser.parse_args()
 
-    def __call__(self, x, train):
-        h = self.c1(F.elu(self.bn1(x, test=not train)))
-        h = self.c2(F.elu(self.bn2(h, test=not train)))
-        shortcut = self.proj(x) if self.use_proj else x
-                          
-        return h + shortcut
-        
+    print('GPU: {}'.format(args.gpu))
+    print('# unit: {}'.format(args.unit))
+    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('# epoch: {}'.format(args.epoch))
+    print('')
 
-class Group(chainer.Chain):
-    def __init__(self, n_layers, in_ch, out_ch, down_sampling=False):
-        super(Group, self).__init__()
-        for i in xrange(n_layers):
-            ch = in_ch if i == 0 else out_ch
-            self.add_link('b{}'.format(i+1), Block(ch, out_ch, down_sampling and i == 0))
+    # Set up a neural network to train
+    # Classifier reports softmax cross entropy loss and accuracy at every
+    # iteration, which will be used by the PrintReport extension below.
+    model = L.Classifier(MLP(args.unit, 2))
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
+        model.to_gpu()  # Copy the model to the GPU
 
-    def __call__(self, x, train):
-        for l in self.children():
-            x = l(x, train)
-        return x
-        
+    # Setup an optimizer
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
 
-class ResNet(chainer.Chain):
+    # Load the MNIST dataset
+    train, test = chainer.datasets.get_cifar10(ndim=1)
 
-    def __init__(self, n_out, n_layers=9):
-        w = math.sqrt(2)
-        super(ResNet, self).__init__(
-            conv1=L.Convolution2D(3, 16, 3, pad=1, stride=1, wscale=w, nobias=True),
-            conv2=Group(n_layers, 16, 16, False),
-            conv3=Group(n_layers, 16, 32, True),
-            conv4=Group(n_layers, 32, 64, True),
-            bn=L.BatchNormalization(64),
-            fc=L.Linear(64, n_out)
-        )
-        self.train = True
+    def filter(dataset):
+        x_ret = []
+        y_ret = []
+        for x, y in dataset:
+            if y in [3, 5]:
+                x_ret.append(x)
+                y_ret.append(0 if y == 3 else 1)
+        return chainer.datasets.TupleDataset(np.asarray(x_ret), np.asarray(y_ret, dtype=np.int32))
+    
+    train = filter(train)
+    test = filter(test)
+                
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+                                                 repeat=False, shuffle=False)
 
-    def __call__(self, x):
-        h = self.conv1(x)
-        h = self.conv2(h, self.train)
-        h = self.conv3(h, self.train)
-        h = self.conv4(h, self.train)
-        h = F.elu(self.bn(h))
-        h = F.average_pooling_2d(h, 8, stride=1)
-        h = self.fc(h)
-        return h
+    # Set up a trainer
+    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
 
-class PreprocessedDataset(chainer.dataset.DatasetMixin):
+    # Evaluate the model with the test dataset for each epoch
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
 
-    def __init__(self, base, crop_size=4, random=True):
-        self.base = base
-        self.crop_size = crop_size
-        self.random = random
+    # Dump a computational graph from 'loss' variable at the first iteration
+    # The "main" refers to the target link of the "main" optimizer.
+    trainer.extend(extensions.dump_graph('main/loss'))
 
-    def __len__(self):
-        return len(self.base)
+    # Take a snapshot at each epoch
+    trainer.extend(extensions.snapshot(), trigger=(args.epoch, 'epoch'))
 
-    def get_example(self, i):
-        # It reads the i-th image/label pair and return a preprocessed image.
-        # It applies following preprocesses:
-        #     - Cropping (random or center rectangular)
-        #     - Random flip
-        #     - Scaling to [0, 1] value
-        crop_size = self.crop_size
+    # Write a log of evaluation statistics for each epoch
+    trainer.extend(extensions.LogReport())
 
-        image, label = self.base[i]
-        _, h, w = image.shape
+    # Print selected entries of the log to stdout
+    # Here "main" refers to the target link of the "main" optimizer again, and
+    # "validation" refers to the default name of the Evaluator extension.
+    # Entries other than 'epoch' are reported by the Classifier link, called by
+    # either the updater or the evaluator.
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss', 'validation/main/loss',
+         'main/accuracy', 'validation/main/accuracy']))
 
-        if self.random:
-            # Randomly crop a region and flip the image
-            top = random.randint(-4, 4)
-            left = random.randint(-4, 4)
-            bottom = top + h
-            right = left + w
-            new_image = np.zeros_like(image)
-            if top < 0:
-                if left < 0:
-                    new_image[:, -top:h, -left:w] = image[:, 0:bottom, 0:right]
-                else:
-                    new_image[:, -top:h, 0:w-left] = image[:, 0:bottom, left:w]
-            else:
-                if left < 0:
-                    new_image[:, 0:h-top, -left:w] = image[:, top:h, 0:right]
-                else:
-                    new_image[:, 0:h-top, 0:w-left] = image[:, top:h, left:w]
-            image = new_image
+    # Print a progress bar to stdout
+    trainer.extend(extensions.ProgressBar())
 
-            if random.randint(0, 1):
-                image = image[:, :, ::-1]
+    if args.resume:
+        # Resume from a snapshot
+        chainer.serializers.load_npz(args.resume, trainer)
 
-        mean = image.mean(axis=(1, 2))
-        var = image.var(axis=(1, 2))
-        var += 1e-10
-        image -= mean[:, np.newaxis, np.newaxis]
-        image /= var[:, np.newaxis, np.newaxis]
-        return image, label
+    # Run the training
+    trainer.run()
 
-parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-parser.add_argument('--gpu', '-g', type=int, default=-1,
-                    help='GPU ID (negative value indicates CPU)')
-args = parser.parse_args()
-
-train, test = chainer.datasets.get_cifar10()
-pre_train = PreprocessedDataset(train)
-pre_test = PreprocessedDataset(test, False)
-
-batchsize = 128
-train_iter =chainer.iterators.SerialIterator(pre_train, batchsize)
-test_iter = chainer.iterators.SerialIterator(pre_test, batchsize,
-                                             repeat=False, shuffle=False)
-
-# model = L.Classifier(MLP(784, 10))
-model = L.Classifier(ResNet(10, 4))
-
-if args.gpu >= 0:
-    chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
-    model.to_gpu()  # Copy the model to the GPU
-
-opt = chainer.optimizers.MomentumSGD(lr=0.1, momentum=0.9)
-opt.use_cleargrads()
-opt.setup(model)
-opt.add_hook(chainer.optimizer.WeightDecay(0.0005))
-
-epoch = 200
-
-# Set up a trainer
-updater = training.StandardUpdater(train_iter, opt, device=args.gpu)
-trainer = training.Trainer(updater, (epoch, 'epoch'), out='result')
-
-# Evaluate the model with the test dataset for each epoch
-trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
-
-# Dump a computational graph from 'loss' variable at the first iteration
-# The "main" refers to the target link of the "main" optimizer.
-trainer.extend(extensions.dump_graph('main/loss'))
-
-# Take a snapshot at each epoch
-trainer.extend(extensions.snapshot(), trigger=(epoch, 'epoch'))
-
-# Write a log of evaluation statistics for each epoch
-trainer.extend(extensions.LogReport())
-
-# Print selected entries of the log to stdout
-# Here "main" refers to the target link of the "main" optimizer again, and
-# "validation" refers to the default name of the Evaluator extension.
-# Entries other than 'epoch' are reported by the Classifier link, called by
-# either the updater or the evaluator.
-trainer.extend(extensions.PrintReport(
-    ['epoch', 'main/loss', 'validation/main/loss',
-     'main/accuracy', 'validation/main/accuracy']))
-
-# Print a progress bar to stdout
-trainer.extend(extensions.ProgressBar())
-
-resume = False
-if resume:
-    # Resume from a snapshot
-    chainer.serializers.load_npz(resume, trainer)
-
-# Run the training
-trainer.run()
+if __name__ == '__main__':
+    main()
