1515efbadedd85a530562444ca1ae06fd6609478
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2016-11-07 11:17:27 -0800

    Fix notations and sample codes in page 4

--- 001introduction/004/example.py
+++ 001introduction/004/example.py
@@ -4,13 +4,12 @@ import numpy as np
 from chainer import Variable
 
 
-lin = L.Linear(100, 20)
-x = Variable(np.ones((10, 100)), dtype=np.float32)
+lin = L.Linear(5, 2)
+x = Variable(np.ones((3, 5), dtype=np.float32))
 y1 = lin(x)
 
 print(x.data)
 print(y1.data)
 
 y2 = F.relu(x)
-
 y3 = F.relu(lin(x))
