3d1789d07069ccb4678e6e2e3dd465b2c6996dee
Ayaka Kume <kume@preferred.jp>
2016-11-24 16:07:58 +0900

    Fix bugs for 002/008/example.py

--- 002chainer/008/example.py
+++ 002chainer/008/example.py
@@ -1,12 +1,13 @@
 from chainer import optimizers
 from chainer import links as L
 from chainer import Variable
+from chainer import Chain
+from chainer import functions as F
 import numpy as np
-
 
 class Linear(Chain):
     def __init__(self):
-        super(MyChain, self).__init__(
+        super(Linear, self).__init__(
             l1=L.Linear(1, 1),
         )
 
@@ -18,15 +19,15 @@ def f(x):
     return 5.*x + 10
 
 x = np.linspace(-10, 10, num=1001)
-y = f(x) + 5.*np.random.randn()
+y = f(x) + 5.*np.random.randn(1001)
 
 model = Linear()
 
 opt = optimizers.SGD()
-opt.Setup(model)
+opt.setup(model)
 for epoch in xrange(100):
-    perm = np.random.perm(len(x))
+    perm = np.random.permutation(len(x))
     for i in xrange(len(x)):
-        x_i = Variable(x[perm[i]])
-        y_i = Variable(y[perm[i]])
+        x_i = Variable(np.array([[x[perm[i]]]],'f'))
+        y_i = Variable(np.array([[y[perm[i]]]],'f'))
         ## Write Here
